51cf3fd72015efbae0c43495d544f0ff71667331
sclamons <sclamons@gmail.com>
2020-03-04 16:17:48 -0800

    Minor or otherwise undocumented automated reinstallation push

--- surface_crns/SurfaceCRNQueueSimulator.py
+++ surface_crns/SurfaceCRNQueueSimulator.py
@@ -86,7 +86,7 @@ def main():
                                  type = 'string', dest = 'manifest_filename',
                             help = "points to a manifest file to simulate")
     available_options.add_option("-v", "--version",
-                  action="store_true", dest="querying_version", default=True,
+                  action="store_true", dest="querying_version", default=False,
                   help="show version and exit")
     (command_line_options, args) = available_options.parse_args(sys.argv)
     if command_line_options.querying_version:
